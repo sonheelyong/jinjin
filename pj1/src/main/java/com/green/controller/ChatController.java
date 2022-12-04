d01fc2c876c760f5f5dc5045560c47e485c9854a
sonheelyong <gmlfyd417@gmail.com>
2022-12-05 01:02:46 +0900

    채팅구현

---
+++ pj1/src/main/java/com/green/controller/ChatController.java
@@ -0,0 +1,37 @@
+package com.green.controller;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
+import com.green.model.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+    
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+
+    @MessageMapping("/message") // /app/message
+    @SendTo("/chatroom/public")
+    public Message receivePublicMessage(@Payload Message message){
+
+
+
+        return message;
+    }
+    
+    @MessageMapping("/private-message")
+    private Message receivePrivateMessage(@Payload Message message){
+        
+
+        simpMessagingTemplate.convertAndSendToUser(message.getReceiverName(),"/private",message); // /user/david/private
+        System.out.println(message);
+        return message;
+    }
+}
