13e84bbc6626015133aa1fc2ec6e2ffb6db601e4
karle9412 <rhlanf7gh53@gmail.com>
2022-12-14 17:51:43 +0900

    first commit

--- pj1/src/main/java/com/green/controller/Controller.java
+++ pj1/src/main/java/com/green/controller/Controller.java
@@ -4,13 +4,12 @@ package com.green.controller;
 import com.green.service.UserService;
 import com.green.vo.UserVo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class Controller {
@@ -36,5 +35,16 @@ public class Controller {
     }
 
 
+<<<<<<< HEAD
+=======
+    @RequestMapping(value= "/User/Registry", method=RequestMethod.POST, produces = "application/json")
+    public void Registry(@RequestBody Map<String, Object> param){
+
+        System.out.println(param);
+
+    }
+
+
+>>>>>>> a4e969a7bd6bfc9f96835b016e7e6379aea44c8f
 
     }
--- pj1/src/main/reactfront/src/App.js
+++ pj1/src/main/reactfront/src/App.js
@@ -1,10 +1,43 @@
+<<<<<<< HEAD
 import React from 'react'
 import ChatRoom from './components/ChatRoom'
+=======
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import About from './components/About';
+import Home from './components/Home';
+import Profile from './components/Profile';
+import SignUp from './components/SignUp';
+import SignUp2 from './components/SignUp2';
+import UserList from './components/UserList';
+import Header from './components/Header'
+import ChatRoom from './components/ChatRoom';
+import Registry from './components/Registry';
+>>>>>>> a4e969a7bd6bfc9f96835b016e7e6379aea44c8f
 
 const App = () => {
   return (
+<<<<<<< HEAD
     <ChatRoom />
   )
 }
+=======
+   <BrowserRouter>
+   <Header/>
+    <Routes>
+     
+     <Route path='/' element={<Home/>} />
+     <Route path='/about' element={<About/>} />
+     <Route path='/profile' element={<Profile/>} />
+     <Route path='/signup' element={<SignUp/>} />
+     <Route path='/signup2' element={<SignUp2/>} />
+     <Route path='/userList' element={<UserList/>} />
+     <Route path='/chatroom' element={<ChatRoom/>} />
+     <Route path='/registry' element={<Registry/>}/>
+     
+   </Routes>
+   </BrowserRouter>
+  );
+  }
+>>>>>>> a4e969a7bd6bfc9f96835b016e7e6379aea44c8f
 
 export default App;
