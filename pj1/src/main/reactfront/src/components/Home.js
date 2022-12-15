0f1b04889583ece189277207ddccc85c4b4e50c6
LSJ <isj19785@gmail.com>
2022-12-15 14:25:17 +0900

    ..test

--- pj1/src/main/reactfront/src/components/Home.js
+++ pj1/src/main/reactfront/src/components/Home.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
 
 function Home() {
+  const navigate = useNavigate();
+
+  const startChat = () => {
+    navigate('/signup');
+  }
+
   return (
-    <div>홈입니다</div>
+    
+    <>
+      <div>
+        환영합니다!<br />
+        <button type='button' onClick={startChat}>시작하기</button>
+      </div>
+    </>
+    
   )
 }
 
